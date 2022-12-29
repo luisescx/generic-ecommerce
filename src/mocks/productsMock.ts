5e1417d61fa8d6a95e1f0b76edf05f567a5f5b06
luisescx <luis.esculapio@live.com>
2022-12-29 15:29:25 -0300

    feat: add productsMock

---
+++ src/mocks/productsMock.ts
@@ -0,0 +1,86 @@
+import { Product } from "types/domain/product";
+
+const productsMockList: Product[] = [
+  {
+    id: 1,
+    name: "Xbox Series S",
+    price: 299.99
+  },
+  {
+    id: 2,
+    name: "Xbox Series X",
+    price: 499.99
+  },
+  {
+    id: 3,
+    name: "Playstation 5",
+    price: 499.99
+  },
+  {
+    id: 4,
+    name: "Xbox Series controller",
+    price: 250
+  },
+  {
+    id: 5,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 6,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 7,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 8,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 9,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 10,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 11,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 12,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 13,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 14,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 15,
+    name: "Playstation 4",
+    price: 300
+  },
+  {
+    id: 16,
+    name: "Playstation 4",
+    price: 300
+  }
+];
+
+export { productsMockList };
