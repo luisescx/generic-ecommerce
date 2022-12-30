859dfa88d39ddde5eac20ae4a77643a92b67ee98
luisescx <luis.esculapio@live.com>
2022-12-30 15:43:41 -0300

    chore: add images to mocks products

--- src/mocks/productsMock.ts
+++ src/mocks/productsMock.ts
@@ -4,82 +4,62 @@ const productsMockList: Product[] = [
   {
     id: 1,
     name: "Xbox Series S",
-    price: 299.99
+    price: 299.99,
+    image: "xbox-series-s"
   },
   {
     id: 2,
     name: "Xbox Series X",
-    price: 499.99
+    price: 499.99,
+    image: "xbox-series-x"
   },
   {
     id: 3,
-    name: "Playstation 5",
-    price: 499.99
+    name: "Playstation 5 + FIFA 23",
+    price: 499.99,
+    image: "ps5-fifa23"
   },
   {
     id: 4,
-    name: "Xbox Series controller",
-    price: 250
+    name: "Playstation 5 + GOW",
+    price: 250,
+    image: "ps5-gow"
   },
   {
     id: 5,
-    name: "Playstation 4",
-    price: 300
+    name: "Playstation 5 + Forbidden West",
+    price: 600,
+    image: "ps5-horizon"
   },
   {
     id: 6,
-    name: "Playstation 4",
-    price: 300
+    name: "Nintendo Switch",
+    price: 300,
+    image: "nintendo-switch"
   },
   {
     id: 7,
-    name: "Playstation 4",
-    price: 300
+    name: "Xbox Series Controller",
+    price: 250,
+    image: "xbox-series-controller"
   },
   {
     id: 8,
     name: "Playstation 4",
-    price: 300
+    price: 300,
+    image: "playstation4"
   },
   {
     id: 9,
-    name: "Playstation 4",
-    price: 300
+    name: "GOW Ragnarok",
+    price: 150,
+    image: "gow-ragnarok"
   },
   {
     id: 10,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 11,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 12,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 13,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 14,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 15,
-    name: "Playstation 4",
-    price: 300
-  },
-  {
-    id: 16,
-    name: "Playstation 4",
-    price: 300
+    name: "Spider Man - Miles Morales",
+    price: 150,
+    image: "spider-man-miles-morales"
   }
 ];
 
