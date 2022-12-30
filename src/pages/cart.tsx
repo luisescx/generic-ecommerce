c92dc2abca961984a64bc169cb4b25b6529c634e
luisescx <luis.esculapio@live.com>
2022-12-30 11:09:37 -0300

    chore: add cart resume in cart pages

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,13 +2,27 @@ import { ICartState } from "@store/modules/cart/types";
 import { IReducersState } from "@store/modules/rootReducer";
 import Base from "@ui/Base";
 import CartCard from "@ui/CartCard";
+import CartTotalCard from "@ui/CartTotalCard";
 import Head from "next/head";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 export default function Cart() {
   const { products } = useSelector<IReducersState, ICartState>(
     (state) => state.cart
   );
+
+  const totalValue = useMemo(() => {
+    return products.reduce((prev, cur) => prev + cur.price * cur.quantity, 0);
+  }, [products]);
+
+  const itemsTotalQuantity = useMemo(() => {
+    return products.reduce((prev, cur) => prev + cur.quantity, 0);
+  }, [products]);
+
+  const productsTotalQuantity = useMemo(() => {
+    return products.length;
+  }, [products]);
 
   return (
     <>
@@ -22,11 +36,25 @@ export default function Cart() {
             <h4 className="mb-6 ml-2">Cart</h4>
           </div>
 
-          <div className="grid grid-cols-[repeat(auto-fill,_minmax(17rem,_1fr))] gap-4">
-            {products.map((product) => (
-              <CartCard key={product.id} product={{ ...product }} />
-            ))}
-          </div>
+          {!!products.length && (
+            <div className="flex flex-row gap-4">
+              <div className="basis-1/4">
+                <CartTotalCard
+                  totalValue={totalValue}
+                  itemsTotalQuantity={itemsTotalQuantity}
+                  productsTotalQuantity={productsTotalQuantity}
+                />
+              </div>
+
+              <div className="basis-10/12">
+                <div className="grid grid-cols-[repeat(auto-fill,_minmax(17rem,_1fr))] gap-4">
+                  {products.map((product) => (
+                    <CartCard key={product.id} product={{ ...product }} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </main>
       </Base>
     </>
